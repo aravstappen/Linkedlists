f943224efcb349958a336321ccefa2414c2e5836
aravstappen <aravsethi2005@gmail.com>
2025-12-22 20:35:03 +0530

    Update sort012s.cpp

--- sort012s.cpp
+++ sort012s.cpp
@@ -1,1 +1,35 @@
-
+Node* segregate(Node* head) {
+        // code here
+        Node*temp=head;
+        Node*zerohead=new Node(-1);
+        Node*onehead=new Node(-1);
+        Node*twohead=new Node(-1);
+        Node* zero=zerohead;
+        Node* one=onehead;
+        Node* two=twohead;
+        while(temp!=NULL){
+            if(temp->data==0){
+                zero->next=temp;
+                zero=zero->next;
+            }
+            else if(temp->data==1){
+                one->next=temp;
+                one=one->next;
+            }
+            else {
+                two->next=temp;
+                two=two->next;
+            }
+            temp=temp->next;
+        }
+        if(onehead->next!=NULL){
+            zero->next=onehead->next;
+        }
+        else{
+            zero->next=twohead->next;
+        }
+        one->next=twohead->next;
+        two->next=NULL;
+        return zerohead->next;
+    }   
+};
